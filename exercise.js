54e7d3dab4ddb92facf988bfd306e27564630054
karapal <106742331+karapal@users.noreply.github.com>
2022-11-29 16:15:22 +0100

    JS exercise

---
+++ exercise.js
@@ -0,0 +1,65 @@
+
+console.log ("---------------Exercise 1---------------\n");
+
+console.log ("String is a line that contains letters, words and numbers.");
+console.log ("Number is a line that only contains numbers. The numbers can be as long as we want.");
+console.log ("Boolean can only be True or False. When an object has a value its True and when it doesn't its False.");
+console.log ("Undefined is a variable that hasn't been defined. For example, a basket that contains an object but we don't know what it is");
+console.log ("Null is a variable that is empty.For example, when we open the basket there is nothing inside.\n")
+
+
+console.log ("---------------Exercise 2---------------\n");
+
+console.log ("A variable is a place where we can store any data we need. For example, a box is a variable and the content of the box is the data.\n")
+
+
+console.log ("---------------Exercise 3---------------\n");
+
+console.log (12 + 20);
+
+
+console.log ("\n---------------Exercise 4---------------\n");
+
+let x1 = 12; 
+console.log (x1)
+
+console.log ("\n---------------Exercise 5---------------\n");
+
+let fullName = "John Doe";
+console.log (fullName)
+
+console.log ("\n---------------Exercise 6---------------\n");
+
+console.log ( 12 - x1);
+
+
+console.log ("\n---------------Exercise 7---------------\n");
+
+let name1 = "john";
+let name2 = "John";
+console.log ( name1 === name2 );
+console.log(name2.toLowerCase ( name1 === name2 ));
+
+
+console.log ("\n---------------Exercise 8---------------\n");
+
+let x2 = 7; 
+if(x2===7) {
+    console.log ("seven")
+};
+
+console.log ("\n---------------Exercise 9---------------\n");
+
+let age = 17;
+let result = age >= 20 ? " User has to go to school" : " User doesn't have to go to school" ;
+
+console.log (result);
+
+
+
+
+
+
+
+
+
